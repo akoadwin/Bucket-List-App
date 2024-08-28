25b14fc3d890f864674d035104077c2d37fc4bf0
Godwin Mat-ao <adwinkoaclc@gmail.com>
2024-08-28 19:19:10 +0800

    pushed from carmen fiesta

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:bucket_list/screens/main_screen.dart';
-import 'package:bucket_list/screens/home_screen.dart';
-import 'package:flutter/material.dart';
+ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
